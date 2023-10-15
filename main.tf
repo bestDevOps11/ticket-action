d54d946405cb8c09764d3708e2099af8d27f3d5d
roussel <rousseltayou6@gmail.com>
2023-10-15 16:27:02 -0400

    wokflows

---
+++ main.tf
@@ -0,0 +1,39 @@
+#edit now as a varaible:
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+provider "aws" {
+  region = var.region
+  #profile = "default"
+}
+
+resource "aws_instance" "my_demo_ec2" {
+  ami           = var.my_ami # us-east-1
+  instance_type = var.instance_type
+  tags = {
+    "Name" = "my_first_ec2"
+  }
+}
+variable "region"{
+  description = "the aws region"
+  type = string
+  defaut = "us-east-1"
+}
+
+variable "my_ami"{
+  description = "the aws ami"
+  type = string
+  defaut = "ami-026ebd4cfe2c043b2"
+}
+
+variable "instance_type"{
+  description = "the ec2 instance type"
+  type = string
+  defaut = "t2.micro"
+}
