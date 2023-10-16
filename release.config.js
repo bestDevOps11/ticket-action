80236a308c74e00385ed1008567b374331d83dcc
roussel <rousseltayou6@gmail.com>
2023-10-16 19:20:02 -0400

    fix: rebase

--- release.config.js
+++ release.config.js
@@ -4,9 +4,9 @@ module.exports = {
     "plugins": [
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
-        ["@semantic-release/git", {
+        "@semantic-release/git", {
           "assets": ["dist/**/*.{js,css}", "docs", "package.json"],
           "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
         }]
- ]
+
 
