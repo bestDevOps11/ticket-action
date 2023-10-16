f7cc81587f8d2a20153a32faec3cf9902cd9195c
roussel <rousseltayou6@gmail.com>
2023-10-16 19:16:26 -0400

    fix: rebase

--- release.config.js
+++ release.config.js
@@ -8,5 +8,5 @@ module.exports = {
           "assets": ["dist/**/*.{js,css}", "docs", "package.json"],
           "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
         }]
-        "@semantic-release/github"]
+ ]
 
