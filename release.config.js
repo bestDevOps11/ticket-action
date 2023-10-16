6e20d76d59745c5d8434912cabc3923060aa8624
roussel <rousseltayou6@gmail.com>
2023-10-16 19:30:50 -0400

    fix: rebase

--- release.config.js
+++ release.config.js
@@ -2,12 +2,9 @@ module.exports = {
     branches: "main",
     repositoryUrl: "https://github.com/bestDevOps11/ticket-action.git",
     "plugins": [
-        "@semantic-release/commit-analyzer",
-        "@semantic-release/release-notes-generator",
-        "@semantic-release/git", {
-          "assets": ["dist/**/*.{js,css}", "docs", "package.json"],
-          "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-        },
-        "@semantic-release/github"]
-
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        '@semantic-release/git',
+        '@semantic-release/github']
+}
 
